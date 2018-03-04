b6a568096627a8ffb7ac9116fbd5f3e5c5938760
olamijinadebayo <consultolamijin@gmail.com>
2018-03-05 00:16:57 +0300

    Adding User logic
    this will add click function to the home page so it can proceed to the game console when clicked

--- js/script.js
+++ js/script.js
@@ -49,4 +49,21 @@ var clearValues = function () {
 }
 
 // user i
-$()
+$(document).ready(function () {
+  $("button#start").click(function () {
+    player1 = new Player(true);
+    player1 = new Player(true);
+    $(".player-console").show();
+    $(".start-menu").hide();
+
+    var player1Name = $(".player1Name").val();
+    $(".player1Name").text(player1Name);
+
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+
+    player1.playerName = player1Name;
+    player2.playerName = player2Name;
+
+  });
+});
