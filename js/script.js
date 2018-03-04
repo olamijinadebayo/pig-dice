cad663ee407893399ace502f892fd86067b0d32d
olamijinadebayo <consultolamijin@gmail.com>
2018-03-04 23:56:49 +0300

    this will add the function to implement the hold functionality and another function to determine the winner of the game

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ var player2 = "";
 var throwdice = function () {
   return Math.ceil(6 * Math.random());
 };
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+//checking if user rolls a value of one
+Player.prototype.rollone = function () {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("Unfortunately" + this.playername + "you rolled a 1! your turn is over")
+  } else {
+    this.tempscore += this.roll;
+  }
+}
