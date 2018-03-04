40f09a2d864917a5fea69fa4030e47026fc99dfb
olamijinadebayo <consultolamijin@gmail.com>
2018-03-04 22:46:32 +0300

    Added specifications to README file
    this will indicate the the objectives the program is expected to achieve at different stages

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 # Anitas Kitchen
-#### A website to help market anita's restaurant so as to increase sales, 18th february 2018
+#### A pig dice game played between two people.
+### 18th february 2018
 #### By **Adebayo Olamijin**
 ## Description
-This is a single page web page for a restaurant built fully with HTML,CSS and Bootstrap framework and JQUERY.The purpose of this page is to help market the restaurant to online customers so as to increase sales.
+A game involving two players whereby each player gets to roll a die a certain number of times,
+the first player to get a total score of 100 is declared as the winner.
 ## Setup/Installation Requirements
 * All you need is a modern browser to view the webpage.
 
-## Known Bugs
-Some portions of the page was designed using the CSS grid system which might not be compatible
-with some very old browsers especially opera mini on mobile phones
+## specifications
+Generate a random number 1-6
+Example Input: function()
+Example Output: 5
+Add roll to scoreTurn
+Example Input: scoreTurn 4, roll 3
+Example Output: scoreTurn 7
+If a 1 is rolled, scoreTurn is 0
+Example Input: roll 1, scoreTurn 10
+Example Output: scoreTurn 0
+When turn is over add scoreTurn to scoreGame
+Example Input: scoreGame 20, scoreTurn 10
+Example Output: scoreGame 30
+When turn is over scoreTurn resets to 0
+Example Input: scoreTurn 10
+Example Output: scoreTurn 0
+If a players scoreGame reaches 100 the game is over and that player wins
+Example Input: scoreGame 105
+Example Output: "Player wins! Game Over"
 
 ## Technologies Used
-HTML,CSS,BOOTSTRAP and JQUERY
+HTML,CSS,BOOTSTRAP,JQUERY and JavaScript
 ## Support and contact details
 contact me at consultolamijin@gmail.com and also feel free to contribute to this project.
 ### License
--- js/script.js
+++ js/script.js
@@ -1,75 +1,178 @@
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-  this.addresses = [];
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+var scores, roundScore, activePlayer, gamePlaying;
+var btnRoll = document.querySelector('.btn-roll');
+var btnHold = document.querySelector('.btn-hold');
+
+//dice images
+var diceimgs = {
+  diceimg01: "https://cdn.pbrd.co/images/70YJMCVVR.png",
+  diceimg02: 'https://cdn.pbrd.co/images/711lemsMX.png',
+  diceimg03: "https://cdn.pbrd.co/images/711NjfjV5.png",
+  diceimg04: "https://cdn.pbrd.co/images/712dK3C2z.png",
+  diceimg05: "https://cdn.pbrd.co/images/70Zqc4icX.png",
+  diceimg06: "https://cdn.pbrd.co/images/712DzRw22.png",
+  diceimg11: "https://cdn.pbrd.co/images/713n3lHQN.png",
+  diceimg12: 'https://cdn.pbrd.co/images/713JSMJDr.png',
+  diceimg13: "https://cdn.pbrd.co/images/HvoZO4Gb.png",
+  diceimg14: "https://cdn.pbrd.co/images/HvqN3Kjq.png",
+  diceimg15: "https://cdn.pbrd.co/images/714IXBStH.png",
+  diceimg16: "https://cdn.pbrd.co/images/714ZovsdD.png"
 };
-Contact.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName;
-}
-
-function Address(street, city, county) {
-  this.street = street;
-  this.city = city;
-  this.county = county;
-}
-Address.prototype.fullAddress = function () {
-  return this.street + ", " + this.city + ", " + this.county;
-}
-
-$(document).ready(function () {
-  $("#add-address").click(function () {
-    $("#new-addresses").append('<div class="new-address">' +
-      '<div class="form-group">' +
-      '<label for="new-street">Street</label>' +
-      '<input type="text" class="form-control new-street">' +
-      '</div>' +
-      '<div class="form-group">' +
-      '<label for="new-city">City</label>' +
-      '<input type="text" class="form-control new-city">' +
-      '</div>' +
-      '<div class="form-group">' +
-      '<label for="new-county">County</label>' +
-      '<input type="text" class="form-control new-county">' +
-      '</div>' +
-      '</div>'
-    )
-  });
-  $("form#new-contact").submit(function (event) {
-    event.preventDefault();
-
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-    // $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-    $(".contact").last().click(function () {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function (address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
-    // resetFields();
-
-    (function resetFields() {
-      $("input#new-first-name").val("");
-      $("input#new-last-name").val("");
-      $("input.new-street").val("");
-      $("input.new-city").val("");
-      $("input.new-county").val("");
-    })();
+init();
 
 
-    $(".new-address").each(function () {
-      var inputtedStreet = $("this").find("input.new-street").val();
-      var inputtedCity = $("this").find("input.new-city").val();
-      var inputtedCounty = $("this").find("input.new-county").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
-      newContact.addresses.push(newAddress);
-    });
-  });
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  if (gamePlaying) {
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = diceimgs['diceimg' + activePlayer + dice];
+
+    document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em';
+
+    // 3. Update round score if the rolled number is not 1
+    if (dice !== 1) {
+      hideRolledMsg();
+      //add score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      //disable button
+
+      disableBtn(btnRoll, 1000);
+      hideRolledMsg();
+      document.querySelector('.player-' + activePlayer + '-rolled-1').style.visibility = 'visible';
+      nextPlayer();
+    }
+  }
+
+
 });
+
+
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  if (gamePlaying) {
+    disableBtn(btnRoll, 1000);
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+
+    //Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    //check if player won the game
+
+    if (scores[activePlayer] >= 50) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner-' + activePlayer);
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active-' + activePlayer);
+      gamePlaying = false;
+
+    } else {
+      nextPlayer();
+    }
+  }
+
+
+});
+
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+document.querySelector('.btn-rules').addEventListener('click', function () {
+  var games = document.getElementsByClassName('game-panel');
+  for (i = 0; i < games.length; i++) {
+    games[i].style.display = 'none';
+  }
+
+  document.querySelector('.btn-back').style.display = 'block';
+  var rules = document.getElementsByClassName('rules-panel');
+  for (i = 0; i < rules.length; i++) {
+    rules[i].style.display = 'block';
+  }
+
+});
+
+document.querySelector('.btn-back').addEventListener('click', function () {
+  var games = document.getElementsByClassName('game-panel');
+  for (i = 0; i < games.length; i++) {
+    games[i].style.display = 'block';
+  }
+
+  document.querySelector('.btn-back').style.display = 'none';
+  var rules = document.getElementsByClassName('rules-panel');
+  for (i = 0; i < rules.length; i++) {
+    rules[i].style.display = 'none';
+  }
+
+});
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-rolled-1').style.visibility = 'hidden';
+  document.querySelector('.player-1-rolled-1').style.visibility = 'hidden';
+
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.add('active-0');
+  document.querySelector('.player-0-panel').classList.remove('winner-0');
+  document.querySelector('.player-1-panel').classList.remove('winner-1');
+
+}
+
+function nextPlayer() {
+  //next player
+  var icons = document.getElementsByTagName('i');
+  for (i = 0; i < icons.length; i++) {
+    icons[i].classList.remove('color-' + activePlayer);
+  }
+
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active-' + activePlayer);
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  for (i = 0; i < icons.length; i++) {
+    icons[i].classList.add('color-' + activePlayer);
+  }
+  document.querySelector('.player-' + activePlayer + '-panel').classList.add('active-' + activePlayer);
+  document.querySelector('#current-0').textContent = '0';
+  document.querySelector('#current-1').textContent = '0';
+}
+
+function disableBtn(btn, time) {
+  //disable button
+  btn.disabled = true;
+  setTimeout(function () {
+    btn.disabled = false;
+  }, time);
+}
+
+function hideRolledMsg() {
+  document.querySelector('.player-0-rolled-1').style.visibility = 'hidden';
+  document.querySelector('.player-1-rolled-1').style.visibility = 'hidden';
+}
