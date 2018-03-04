42086b2f15eb92f60d8a5a87607706449a6dafa1
olamijinadebayo <consultolamijin@gmail.com>
2018-03-05 00:40:19 +0300

    this will add a function to implement the hold functionality whenever the hold button is clicked

--- js/script.js
+++ js/script.js
@@ -92,4 +92,18 @@ $(document).ready(function () {
     player2.rollone();
     $("#round-total-2").text(player2.tempscore);
   });
+  $("button#player1-hold").click(function () {
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+    $("#die-roll-1").empty();
+    player1.winnerCheck();
+  });
+  $("button#player2-hold").click(function (event) {
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#die-roll-2").empty();
+    player2.winnerCheck();
+  });
 });
